7e77632b3438cb002b196be8fc4b366559f9a057
Malsone <marcllabco@gmail.com>
2021-05-08 17:37:09 +0200

    Nuevo intento proyecto heroku+mongoDB

--- src/controllers/posts.controller.js
+++
@@ -1,9 +0,0 @@
-import views from '../views/posts.html'
-
-export default () => {
-
-    const divElement = document.createElement('div');
-    divElement.innerHTML = views;
-
-    return divElement;
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,42 @@
-// Ejecuta el archivo mian.js y lo crea de nuevo en una carpeta llamada dist con el nombre bundle.js
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  // añadido buscando en google CUIDADO FUNCIONA**********************
-    mode: 'development',
-    devtool: 'eval',
-    cache: true,
-    performance: {
-      hints: false
-    },
-  // añadido buscando en google CUIDADO FUNCIONA**********************
-    entry: './src/main.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.html$/i,
-            loader: 'html-loader',
-          },
-          {
-            // Webpack loader de css
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
-          },
-          {
-            // Webpack loader de sass
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              "style-loader",
-              // Translates CSS into CommonJS
-              "css-loader",
-              // Compiles Sass to CSS
-              "sass-loader"
-            ]
-          }
-        ],
-      },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
+  entry: './frontend/app.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'backend/public'),
+    filename: 'js/bundle.js'
+  },
+  module : {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
     ]
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './frontend/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/bundle.css"
+    })
+  ],
+  devtool: 'source-map'
+};
